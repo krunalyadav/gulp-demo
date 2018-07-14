39089e5fce6d97d66c2d9c43c53361df9fae13ff
Krunal Yadav <krunal.yadav18@gmail.com>
2018-07-14 23:14:44 +0530

    Add css js injection via gulp

--- gulp.config.js
+++ gulp.config.js
@@ -1,9 +1,34 @@
 module.exports = function() {
-    var client = './src/client';
+    var client = "./src/client/";
+    var clientApp = client + "app/";
+    var temp = "./.temp/";
     var config = {
-        alljs: ['./src/**/*.js', './*.js'],
-        temp: './.temp/',
-        less: client + '/styles/styles.less'
+        alljs: ["./src/**/*.js", "./*.js"],
+        temp: temp,
+        less: client + "/styles/styles.less",
+        client: client,
+        index: client + "index.html",
+        css: temp + "styles.css",
+        js: [
+            clientApp + "**/*.module.js",
+            clientApp + "**/*.js",
+            "!" + clientApp + "**/*.spec.js"
+        ],
+
+        bower: {
+            json: require("./bower.json"),
+            directory: "./bower_components",
+            ignorePath: "../.."
+        }
+    };
+
+    config.getWiredepDefaultOptions = function() {
+        var options = {
+            bowerJson: config.bower.json,
+            directory: config.bower.directory,
+            ignorePath: config.bower.ignorePath
+        };
+        return options;
     };
 
     return config;
--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,65 @@
-var $ = require('gulp-load-plugins')({ lazy: true });
-var gulp = require('gulp');
-var config = require('./gulp.config')();
-var del = require('del');
+var $ = require("gulp-load-plugins")({ lazy: true });
+var gulp = require("gulp");
+var config = require("./gulp.config")();
+var del = require("del");
 
-gulp.task('vet', function() {
-    log('Analyzing source with JSHint and JSCS');
+gulp.task("vet", function() {
+    log("Analyzing source with JSHint and JSCS");
 
     gulp.src(config.alljs)
         .pipe($.jscs())
         .pipe($.jshint())
-        .pipe($.jshint.reporter('jshint-stylish', { verbose: true }))
-        .pipe($.jshint.reporter('fail'));
+        .pipe($.jshint.reporter("jshint-stylish", { verbose: true }))
+        .pipe($.jshint.reporter("fail"));
 });
 
-gulp.task('styles', function() {
-    log('Compiling Less --> CSS');
+gulp.task("styles", function() {
+    log("Compiling Less --> CSS");
 
     return gulp
         .src(config.less)
         .pipe($.plumber())
         .pipe($.less())
-        .pipe($.autoprefixer({ browsers: ['last 2 version', '> 5%'] }))
+        .pipe($.autoprefixer({ browsers: ["last 2 version", "> 5%"] }))
         .pipe(gulp.dest(config.temp));
 });
 
-gulp.task('clean-styles', function(done) {
-    var files = config.temp + '**/*.css';
+gulp.task("clean-styles", function(done) {
+    var files = config.temp + "**/*.css";
     clean(files, done);
 });
 
-gulp.task('less-watcher', function() {
-    gulp.watch([config.less], ['styles']);
+gulp.task("less-watcher", function() {
+    gulp.watch([config.less], ["styles"]);
+});
+
+gulp.task("wiredep", function() {
+    log('Wire up the bower css js and our app js into the html');
+    var options = config.getWiredepDefaultOptions();
+    var wiredep = require("wiredep").stream;
+
+    return gulp
+        .src(config.index)
+        .pipe(wiredep(options))
+        .pipe($.inject(gulp.src(config.js)))
+        .pipe(gulp.dest(config.client));
+});
+
+gulp.task("inject", ["wiredep","styles"], function() {
+    log('Wire up the app css into the html, and call wiredep');
+    return gulp
+        .src(config.index)
+        .pipe($.inject(gulp.src(config.css)))
+        .pipe(gulp.dest(config.client));
 });
 
 function clean(path, done) {
-    log('Cleaning: ' + $.util.colors.blue(path));
+    log("Cleaning: " + $.util.colors.blue(path));
     del(path, done);
 }
 
 function log(msg) {
-    if (typeof msg === 'object') {
+    if (typeof msg === "object") {
         for (var item in msg) {
             if (msg.hasOwnProperty(item)) {
                 $.util.log($.util.colors.blue(msg[item]));
